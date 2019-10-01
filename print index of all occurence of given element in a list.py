c1d5b1c9a5514b7a2fa86a5710265b347de6eb47
Hemant Gangwar <34183672+spidohemant@users.noreply.github.com>
2019-10-02 00:31:45 +0530

    Delete print index of all occurence of given element in a list.py

--- print index of all occurence of given element in a list.py
+++
@@ -1,9 +0,0 @@
-#practice of basic python programming
-
-l=[eval(x) for x in input("Enter list Elements").split(',')]
-element=eval(input("Enter element value :"))
-index=0
-while index<len(l) :
-	if l[index]==element :
-		print(index,end=' ')
-	index+=1

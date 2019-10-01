bbef59e87db59f5fd5f02148bcaca0988d42af4e
Hemant Gangwar <34183672+spidohemant@users.noreply.github.com>
2019-10-02 00:53:27 +0530

    Add files via upload

---
+++ show message.py
@@ -0,0 +1,10 @@
+from tkinter import*
+from tkinter import messagebox
+def call():
+	messagebox.showinfo("Succcesfull","Installation Completed")
+root=Tk()
+b=Button(root,text="Click",command=call)
+b.pack()
+
+root.mainloop()
+os.system("pause")

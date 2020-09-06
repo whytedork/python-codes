40c1650ad59b5a598bd30b5a094541744cfdf553
Hemant Gangwar <34183672+whytedork@users.noreply.github.com>
2020-09-06 16:35:22 +0530

    decorator

---
+++ decorator.py
@@ -0,0 +1,16 @@
+# Demonstrating the Concept of Decorators using example
+
+# instagram.com/machine_learning.teknoboost
+
+def div(a,b):
+	print(a/b)
+
+def smart_div(func):
+	def inner(a,b):
+		if a<b:
+			a,b=b,a
+		return func(a,b)
+	return inner
+
+div=smart_div(div)
+div(2,4)
